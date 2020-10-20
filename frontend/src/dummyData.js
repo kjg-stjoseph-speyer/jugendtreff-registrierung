726c5f1c4a6fb3085f604d44d34321666328a862
Steffen Schön <steffen1995@gmail.com>
2020-10-20 15:04:00 +0200

    better dummy data

--- frontend/src/dummyData.js
+++ frontend/src/dummyData.js
@@ -1,48 +1,126 @@
 export const testEvents = [
   {
     id: 1,
-    time: 1603034348156,
+    time: 1603472400000,
     maxParticipants: 15,
     inCharge: "Simon",
     participants: [
       {
-        name: "Hans",
-        timePreference: 1603034348156,
-        userid: 12345678,
+        name: "Jonas",
+        timePreference: 1603472400000,
+        userid: 1,
         waiting: false
       },
       {
-        name: "Peter",
-        timePreference: 1603034348156,
-        userid: 12345672,
+        name: "Laura",
+        timePreference: 1603472400000,
+        userid: 2,
         waiting: false
       },
       {
-        name: "Hans LangerNachname",
-        timePreference: 1603034348156,
-        userid: 12345671,
+        name: "Emil",
+        timePreference: 1603472400000,
+        userid: 3,
         waiting: false,
       },
       {
-        name: "Waiter",
-        timePreference: 1603034348156,
-        userid: 12345673,
+        name: "Emilia",
+        timePreference: 1603472400000,
+        userid: 4,
+        waiting: false,
+      },
+      {
+        name: "Linus",
+        timePreference: 1603472400000,
+        userid: 5,
+        waiting: false,
+      },
+      {
+        name: "Anna",
+        timePreference: 1603472400000,
+        userid: 6,
+        waiting: false,
+      },
+      {
+        name: "Lukas",
+        timePreference: 1603472400000,
+        userid: 7,
+        waiting: false,
+      },
+      {
+        name: "Julia",
+        timePreference: 1603472400000,
+        userid: 8,
+        waiting: false,
+      },
+      {
+        name: "Thomas",
+        timePreference: 1603472400000,
+        userid: 9,
+        waiting: false,
+      },
+      {
+        name: "Ella",
+        timePreference: 1603472400000,
+        userid: 10,
+        waiting: false,
+      },
+      {
+        name: "Felix",
+        timePreference: 1603472400000,
+        userid: 11,
+        waiting: false,
+      },
+      {
+        name: "Lina",
+        timePreference: 1603472400000,
+        userid: 12,
+        waiting: false,
+      },
+      {
+        name: "Alexander",
+        timePreference: 1603472400000,
+        userid: 13,
+        waiting: false,
+      },
+      {
+        name: "Leonie",
+        timePreference: 1603472400000,
+        userid: 14,
+        waiting: false,
+      },
+      {
+        name: "Paul",
+        timePreference: 1603472400000,
+        userid: 15,
+        waiting: false,
+      },
+      {
+        name: "Lena",
+        timePreference: 1603472400000,
+        userid: 16,
         waiting: true,
       }
     ]
   },
   {
     id: 2,
-    time: 1603034348156,
+    time: 1603823400000,
     maxParticipants: 10,
     inCharge: "Simon",
     participants: [
+      {
+        name: "Jonas",
+        timePreference: 1603823400000,
+        userid: 1,
+        waiting: true,
+      }
     ]
   },
   {
     id: 3,
-    time: 1603034348156,
-    maxParticipants: 10,
+    time: 1604088000000,
+    maxParticipants: 15,
     inCharge: "Simon",
     participants: [
     ]
